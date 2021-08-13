1b7e14d7900dabf2cf226a0145a6b9aef56ed704
Lucas Soares <lucas.s.gomes@ufv.br>
2021-08-13 08:56:14 -0300

    Finish count list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum_length(list, 0)
+
+  defp sum_length([], acc), do: acc
+
+  defp sum_length([_head | tail], acc) do
+    sum_length(tail, acc + 1)
   end
 end
